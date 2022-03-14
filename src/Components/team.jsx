d791e76e1ab51c9e24a700784e10f5da046f4eb6
Aliyan Hussain <hussainaliyan531@gmail.com>
2022-03-14 22:42:11 +0500

    .

--- src/Components/team.jsx
+++ src/Components/team.jsx
@@ -16,7 +16,13 @@ const Team = () => {
                         <div className="left text-start flex-wrap izp px-0 px-lg- px-md-4 mx-0 mx-lg-4 mx-md-4" data-aos="fade-down">
                             <h2 style={{ fontSize: '22px' }} className="fw-normal"> <img src="images/dot.jpg" className="pl-3" width="10px" height="10px" alt="" />&nbsp;Worry Monsters Founder</h2>
                             <p style={{ fontSize: '15px', lineHeight: '22px' }} className="w-100 pt-2 fw-light">
-                                Artist, Director and University Acting Teacher with a background in Cognitive Behavioural Therapy and interactive game design. Stephen has decades of experience teaching creativity in emotionally-rich, trauma-informed learning environments, fostering personal growth and transformation. To me, the most important step of any path to improvement is confidence and agency. You have to feel that you can do something before you can talk yourself into trying it.
+                                Artist, Director and University Acting Teacher with a background in Cognitive Behavioural Therapy and interactive game design. Stephen has decades of experience teaching creativity in emotionally-rich, trauma-informed learning environments, fostering personal growth and transformation.
+                                <br />
+                                <br />
+                                To me, the most important step of any path to improvement is confidence and agency. You have to feel that you can do something before you can talk yourself into trying it.
+                                <br />
+                                <br />
+                                <b style={{fontWeight: "bolder", color: "black", fontStyle: "italic"}}>“Worrying does not take away tomorrow's troubles. It takes away today's peace.” </b>
                             </p>
                         </div>
                         <div className="right d-flex justify-content-start align-items-center my-1 izi mx-5 px-5" data-aos="flip-right">
